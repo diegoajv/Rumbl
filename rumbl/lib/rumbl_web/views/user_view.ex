8f26d8147e08ab044e70702b7eb6eb551e90a57f
diegoajv <diegojosevilla@outlook.com>
2019-01-21 15:31:07 -0600

    Refactor[user]: Update user from model to context

--- rumbl/lib/rumbl_web/views/user_view.ex
+++ rumbl/lib/rumbl_web/views/user_view.ex
@@ -1,8 +1,9 @@
 defmodule RumblWeb.UserView do
   use RumblWeb, :view
-  alias Rumbl.User
 
-  def first_name(%User{name: name}) do
+  alias Rumbl.Accounts
+
+  def first_name(%Accounts.User{name: name}) do
     name
     |> String.split(" ")
     |> Enum.at(0)
